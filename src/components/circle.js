b451343b298ab68b7d6d55a26e90627aa37e4544
renevanderark <r.van.der.ark@gmail.com>
2016-02-22 11:07:43 +0100

    adds draggable ho component and touch events to grid

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,13 +1,4 @@
 import React from "react";
 import draggable from "./draggable";
-/*
-class Circle extends React.Component {
 
-	render() {
-		return (
-			
-		);
-	}
-}
-*/
-export default draggable(() => (<svg height="40" width="40"><g transform="translate(20 20)"><circle r="20" /></g></svg>), () => (<circle r="20" />));
+export default draggable(() => (<svg height="40" width="40"><g transform="translate(20 20)"><circle r="20" /></g></svg>), () => (<circle r="20" onClick={() => console.log("test")}/>));
